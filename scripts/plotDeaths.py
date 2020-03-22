a4c50fd2ae2ee158d53aa00e318222d2ec2980b3
Peter Shellito <shellito@gmail.com>
2020-03-22 00:20:56 -0700

    Allow zoom-in to Europe

--- scripts/plotDeaths.py
+++ scripts/plotDeaths.py
@@ -11,8 +11,10 @@ dailyDataDir = '../csse_covid_19_data/csse_covid_19_daily_reports/'
 # Directory in which to place figures
 figDir = '../figures/'
 
+# Region to plot
+region = 'europe'
 # Date to plot
-dateToPlot = dt.date(2020,3,20)
+dateToPlot = dt.date(2020,3,21)
 dateStr = dateToPlot.strftime('%m-%d-%Y')
 # Calculate old date time
 oldDate = dateToPlot - dt.timedelta(days=1)
@@ -59,24 +61,47 @@ markerScale = 0.1
 # Figure size
 width = 21
 height = 9
-# Map extent
-minLon = -125
-maxLon = -67
-minLat = 25
-maxLat = 53
-minLon = -168
-maxLon = 180
-minLat = -57
-maxLat = 75
 # Open figure
 mapFig = plt.figure(figsize=(width, height))
 # Open axis
 ax = mapFig.gca()
+# Map extent
+if region == 'northAmerica':
+    # North America
+    minLon = -125
+    maxLon = -67
+    minLat = 25
+    maxLat = 53
+elif region == 'globe':
+    # Globe, pretty much
+    minLon = -168
+    maxLon = 180
+    minLat = -57
+    maxLat = 75
+    # Projection
+    mm = Basemap(projection='eck4', lon_0=0, resolution='l') # Available 'c','l','i','h','f'
+elif region == 'europe':
+    minLon = -05.
+    maxLon = 50.
+    ctrLon = (minLon + maxLon) / 2
+    minLat = 40
+    maxLat = 50
+    ctrLat = (minLat + maxLat) / 2
+    mapWidth = 7000000
+    mapHeight = 4000000
+    ###mm = Basemap(llcrnrlon=minLon, llcrnrlat=minLat, urcrnrlon=maxLon, urcrnrlat=maxLat, resolution='l', area_thresh=1000., projection='poly', lat_0=ctrLat, lon_0=ctrLon, ) # Available 'c','l','i','h','f'
+    #mm = Basemap(width=8000000, height=7000000, resolution='l', projection='aea', lat_1=minLat, lat_2=maxLat, lon_0=ctrLon, lat_0=ctrLat) # Available 'c','l','i','h','f'
+    mm = Basemap(width=mapWidth, height=mapHeight, resolution='l', projection='aea', lat_1=minLat, lat_2=maxLat, lon_0=ctrLon, lat_0=ctrLat) # Available 'c','l','i','h','f'
+    # Placement of credit text
+    txx, tyy = 3500000, 10000
+    print(txx)
+else:
+    raise Exception('requested region not defined.')
+
 # Set up map projection with coastlines
 # Try Albers Equal Area Projection?
 #mm = Basemap(projection='cyl', llcrnrlat=minLat, urcrnrlat=maxLat, \
 #        llcrnrlon=minLon, urcrnrlon=maxLon, resolution='l', lon_0=0) # Available 'c','l','i','h','f'
-mm = Basemap(projection='eck4', lon_0=0, resolution='l') # Available 'c','l','i','h','f'
 # Add country lines
 mm.drawcountries(color='darkgray', zorder=2)
 # Add state lines
@@ -90,6 +115,8 @@ mm.drawmapboundary(fill_color='lightsteelblue')
 # Initialize Total deaths and Total new deaths
 totDeaths = 0
 totNewDeaths = 0
+totRegionDeaths= 0
+totNewRegionDeaths = 0
 # Loop through locations to display
 for ii in range(0,len(idcsToPlot)):
     # Extract coordinates of these data
@@ -97,6 +124,11 @@ for ii in range(0,len(idcsToPlot)):
     lat = newDf.iloc[idcsToPlot[ii]]['Latitude']
     # Convert lon and lat to x and y coordinates in the projection
     xx, yy = mm(lon, lat)
+    # Determine if the label is on the map
+    if xx<0 or yy<0 or xx>mapWidth or yy>mapHeight:
+        labelIsOnMap = False
+    else:
+        labelIsOnMap = True
     # Extract the number of deaths and new deaths in this location
     deaths = newDf.iloc[idcsToPlot[ii]]['Deaths']
     newDeaths = newDf.loc[idcsToPlot[ii],'newDeaths']
@@ -104,21 +136,33 @@ for ii in range(0,len(idcsToPlot)):
     scaledVal = deaths*markerScale
     if deaths > 0:
         ax.scatter(xx, yy, s=deaths, marker='o', linewidth=2, facecolors='none', edgecolors='sienna', label='Deaths', zorder=3)
+    else:
+        pass
     if newDeaths > 0:
         ax.scatter(xx, yy, s=newDeaths, marker='o', facecolors='r', edgecolors='none', label='New deaths', zorder=3)
-    if deaths > 10 and newDeaths > 0 and deaths!=newDeaths:
-        textStr = str(deaths) + ' (' + str(newDeaths) + ')'
-    elif newDeaths > 2:
-        textStr = '(' + str(newDeaths) + ')'
-    elif deaths > 10:
-        textStr = str(deaths)
+    else:
+        pass
+    if labelIsOnMap:
+        #print('xx is ' + str(xx))
+        #print('yy is ' + str(yy))
+        if deaths > 10 and newDeaths > 0 and deaths!=newDeaths:
+            textStr = str(deaths) + ' (' + str(newDeaths) + ')'
+        elif newDeaths > 2:
+            textStr = '(' + str(newDeaths) + ')'
+        elif deaths > 10:
+            textStr = str(deaths)
+        else:
+            textStr = ''
+        #print('label is ' + textStr)
+        totRegionDeaths += deaths
+        totNewRegionDeaths += newDeaths
     else:
         textStr = ''
     ax.text(xx, yy, textStr)
     totDeaths += deaths
     totNewDeaths += newDeaths
 
-plt.title(str(totDeaths) + ' COVID-19 deaths as of ' + dateStr + ' (' + str(totNewDeaths) + ' new since ' + oldDateStr +') Source: Johns Hopkins University')
+plt.title(str(totRegionDeaths) + ' COVID-19 deaths in this box. (' + str(totNewRegionDeaths) + ' on ' + dateStr + ') Source: Johns Hopkins University')
 # Create dummy scatter items for the legend
 lxx, lyy = mm(-99,-99)
 ss1 = ax.scatter(lxx, lyy, s=100, marker='o', linewidth=2, facecolors='none', edgecolors='sienna', zorder=-1)
@@ -126,10 +170,9 @@ ss2 = ax.scatter(lxx, lyy, s=50, marker='o', facecolors='r', edgecolors='none', 
 # Add legend
 ax.legend((ss1, ss2),('Deaths to-date', '(New deaths)'), loc='upper left')
 # Add github source
-txx, tyy = mm(0,-87)
 ax.text(txx, tyy, 'https://github.com/pshellito/COVID-19/tree/deathFigures', ha = 'center', va='bottom')
 # Print the figure
-figName = figDir + dateStr + '_covid19deaths.png'
+figName = figDir + region + '_' + dateStr + '_covid19deaths.png'
 print('Printing ' + figName + '...')
 plt.savefig(figName, bbox_inches='tight')
 
